03d9ef865d58bb6e616bf2b0b70f65dde5bb7448
Ashwini Mhaske <97236860+ashwinimhaske2003@users.noreply.github.com>
2024-04-11 10:16:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let categoryListDiv = document.querySelector(".categoryList");
 let allCatName = [];
 let cart = JSON.parse(localStorage.getItem('cart')) || {};
 
+
 let displayProduct = async (allCheckCat = [], searchText = '') => {
     productDiv.innerHTML = '';
     
@@ -12,45 +13,57 @@ let displayProduct = async (allCheckCat = [], searchText = '') => {
     finalProduct?.categories.forEach((element) => {
         if (!allCatName.includes(element.category_name)) {
             categoryListDiv.innerHTML += ` <label>
-                <input type="checkbox" onclick='categoryFilter()' value="${element.category_name}"> &nbsp; ${element.category_name}
+                <input type="checkbox" onclick='categoryFilter(this)' value="${element.category_name}"> &nbsp; ${element.category_name}
             </label>`
             allCatName.push(element.category_name)
-            
         }
 
         if (allCheckCat.length == 0 || allCheckCat.includes(element.category_name)) {
-            
             if (element.category_name.toLowerCase() === searchText.toLowerCase()) {
-                element.category_products.forEach((item) => {
-                    productDiv.innerHTML += `<div class="productItems">
-                        <img src=${item.image}>
-                        <h6>${item.vendor}</h6>
-                        <hr/>
-                        <h6>${item.badge_text === null ? "" : item.badge_text}</h6>
-                        <h6>Price Rs. ${item.price} | <span>${item.compare_at_price}</span></h6>
-                        <h5>${item.title}</h5>
-                        <button class="addToCartButton" onclick="addToCart('${item.title}', ${item.price})">Add to Cart</button>
-                    </div>`;
-                });
+                renderProducts(element.category_products, searchText);
             } else {
-                element.category_products.forEach((item) => {
-                    if (item.title.toLowerCase().includes(searchText.toLowerCase()) ||
-                        item.vendor.toLowerCase().includes(searchText.toLowerCase())) {
-                        productDiv.innerHTML += `<div class="productItems">
-                            <img src=${item.image}>
-                            <h6>${item.vendor}</h6>
-                            <hr/>
-                            <h6>${item.badge_text === null ? "" : item.badge_text}</h6>
-                            <h6>Price Rs. ${item.price} | <span>${item.compare_at_price}</span></h6>
-                            <h5>${item.title}</h5>
-                            <button class="addToCartButton" onclick="addToCart('${item.title}', ${item.price})">Add to Cart</button>
-                        </div>`;
-                    }
-                });
+                const filteredProducts = element.category_products.filter(item =>
+                    item.title.toLowerCase().includes(searchText.toLowerCase()) ||
+                    item.vendor.toLowerCase().includes(searchText.toLowerCase())
+                );
+                renderProducts(filteredProducts, searchText);
             }
         }
-    })
+    });
 }
+
+
+const renderProducts = (products, searchText) => {
+    products.forEach((item) => {
+        productDiv.innerHTML += `<div class="productItems">
+            <img src=${item.image}>
+            <h6>${item.vendor}</h6>
+            <hr/>
+            <h6>${item.badge_text === null ? "" : item.badge_text}</h6>
+            <h6>Price Rs. ${item.price} | <span>${item.compare_at_price}</span></h6>
+            <h5>${item.title}</h5>
+            <button class="addToCartButton" onclick="addToCart('${item.title}', ${item.price})">Add to Cart</button>
+        </div>`;
+    });
+}
+
+
+const categoryFilter = (checkbox) => {
+    const checkedCategories = Array.from(document.querySelectorAll('.categoryList input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
+    const searchText = document.getElementById('searchInput').value;
+    displayProduct(checkedCategories, searchText);
+};
+
+
+
+document.getElementById('searchInput').addEventListener('input', () => {
+    const checkedCategories = Array.from(document.querySelectorAll('.categoryList input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
+    const searchText = document.getElementById('searchInput').value;
+    displayProduct(checkedCategories, searchText);
+});
+
+
+
 
 let addToCart = (productName, productPrice) => {
     if (cart[productName]) {
@@ -66,13 +79,10 @@ let addToCart = (productName, productPrice) => {
     console.log(`${productName} added to cart.`);
 };
 
+
 document.getElementById('viewCartButton').addEventListener('click', () => {
     window.location.href = 'cart.html'; 
 });
 
 
-
-
 displayProduct();
-
-
